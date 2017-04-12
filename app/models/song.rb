c75ec3a28104a3cbf678cbfcb6d730baad44d09d
meryldakin <meryl.k.dakin@gmail.com>
2017-04-12 15:26:25 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,8 +1,8 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }, allow_nil: true
+  validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }, allow_nil: true, presence: true, if: :released?
   validates :title, uniqueness: { scope: [:release_year, :artist_name]}
-  validates :release_year, presence: true, if: :released?
+  # validates :release_year, presence: true, if: :released?
 
 
 end
